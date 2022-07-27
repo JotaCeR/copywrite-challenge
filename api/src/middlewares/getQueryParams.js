4733e63821c02ae402cd0b6896de31f015d0af6b
Juan Roberts <juancroberts31@gmail.com>
2022-07-27 18:55:44 -0300

    Polished test cases + polished middleware

--- api/src/middlewares/getQueryParams.js
+++ api/src/middlewares/getQueryParams.js
@@ -1,13 +1,6 @@
 function getQueryParams (req, res, next) {
     try {
-        const params = req.query;
-        const check = Object.entries(params);
-
-        if (check.length <= 0) {
-            return res.status(400).send('API needs query params passed by to function correctly...')
-        } else if (!params.text) {
-            return res.status(400).json({error: 'no text'})
-        }
+        if (!req.query.text) return res.status(400).json({error: 'no text'})
 
         next();
     } catch (e) {
--- api/tests/responses.spec.js
+++ api/tests/responses.spec.js
@@ -3,55 +3,48 @@ const request = require('supertest')
 const app = require('../src/app.js')
 
 describe('Valid Requests', () => {
-  it('should get 200', () => {
-    request(app).get('/iecho?text=casa')
-      .set('Accept', 'application/json')
-      .expect(200)
+  it('should get 200', async () => {
+    const response = await request(app).get('/iecho?text=casa').set('Accept', 'application/json')
+      expect(response.status).to.equal(200);
   })
 
-  it('should get a reverse text', () => {
-    request(app).get('/iecho?text=calificacion')
-      .set('Accept', 'application/json')
-      .expect(function(res) {
-        expect(res.body.text).to.equal('noicacifilac')
-      })
+  it('should get a reverse text', async () => {
+    const response = await request(app).get('/iecho?text=calificacion').set('Accept', 'application/json')
+      expect(response.body.text).to.equal('noicacifilac')
     });
 
-  it('should get a reverse text no matter the length', () => {
-    request(app).get('/iecho?text=abcdefghijklmnopqrstuvwxyz')
-      .set('Accept', 'application/json')
-      .expect(function(res) {
-        expect(res.body.text).to.equal('zyxwvutsrqponmlkjihgfedcba')
-    })
+  it('should get a reverse text no matter the length', async () => {
+    const response = await request(app).get('/iecho?text=abcdefghijklmnopqrstuvwxyz').set('Accept', 'application/json')
+    
+      expect(response.body.text).to.equal('zyxwvutsrqponmlkjihgfedcba')
   });
 
-  it('should respond with json', () => {
-    request(app).get('/iecho?text=auto')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
+  it('should respond with json', async () => {
+    const response = await request(app).get('/iecho?text=auto').set('Accept', 'application/json')
+      expect(response.headers['content-type'], /json/);
   })
 })
 
 describe('Palindrome Requests', () => {
-  it('should get 200', () => {
-    request(app).get('/iecho?text=neuquen')
-      .set('Accept', 'application/json')
-      .expect(200)
+  it('should get 200', async () => {
+    const response = await request(app).get('/iecho?text=neuquen').set('Accept', 'application/json')
+      expect(response.status).to.equal(200)
   })
 
-  it('should get a "palindrome" flag set on true in the response', () => {
-    request(app).get('/iecho?text=neuquen')
-      .set('Accept', 'application/json')
-      .expect(function(res) {
-        expect(res.body.palindrome).to.equal(true)
-  })
+  it('should get a "palindrome" flag set on true in the response', async () => {
+    const response = await request(app).get('/iecho?text=neuquen').set('Accept', 'application/json')
+      expect(response.body.palindrome).to.equal(true)
 })
 
 describe('Invalid Requests', () => {
-  it('should get 400', () => {
-    request(app).get('/iecho')
-      .set('Accept', 'application/json')
-      .expect(400)
+  it('should get 400', async () => {
+    const response = await request(app).get('/iecho').set('Accept', 'application/json')
+      expect(response.status).to.equal(400)
+  })
+
+  it('should get error text', async () => {
+    const response = await request(app).get('/iecho').set('Accept', 'application/json')
+      expect(response.body.error).to.equal('no text')
   })
 })
 })
