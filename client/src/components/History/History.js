ea22a7af52313c9f5b5b77a96fea05132ecd5df2
Juan Roberts <juancroberts31@gmail.com>
2022-07-28 17:56:56 -0300

    Re-factor client testing. Time issues

---
+++ client/src/components/History/History.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export default function History () {
+
+    return (
+        <Fragment>
+            <h1>Results:</h1>
+            <ul>
+                <li>horse</li>
+            </ul>
+        </Fragment>
+    )
+}
