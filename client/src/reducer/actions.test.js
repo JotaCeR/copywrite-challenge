ef7bf57074cf47e503f6029f8e904b90914f6e9e
Juan Roberts <juancroberts31@gmail.com>
2022-07-28 05:14:39 -0300

    Primal TDD/BDD setup

---
+++ client/src/reducer/actions.test.js
@@ -0,0 +1,29 @@
+import { search, clean as currentClean } from './currentSlice'
+import { add, remove } from './historySlice'
+import { update, clean as lastClean } from './lastSlice'
+
+describe('Action Creators', () => {
+    it('', () => {
+
+    })
+
+    it('', () => {
+
+    })
+
+    it('', () => {
+
+    })
+
+    it('', () => {
+
+    })
+
+    it('', () => {
+
+    })
+
+    it('', () => {
+        
+    })
+})
