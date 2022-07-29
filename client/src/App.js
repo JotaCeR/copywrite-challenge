487d029f56f6a2ab0020876e713fd45e6ff03677
Juan Roberts <juancroberts31@gmail.com>
2022-07-28 22:09:39 -0300

    ThemeProvider built + Google Fonts setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import DefaultHeader from './components/DefaultHeader';
+import Home from './layouts/Home'
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#C0392B'
+      },
+      secondary: {
+        main: '#4E1FBA'
+      },
+      background: {
+        main: '#F0EAD6'
+      },
+      error: {
+        main: '#FF0000'
+      }
+    },
+    typography: {
+      fontFamily: [
+        'Poppins',
+        'sans-serif'
+      ].join(','),
+      fontSize: 12
+    },
+    spacing: 3
+  })
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<DefaultHeader />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme} >
+      <div className="App">
+        <Routes>
+          <Route exact path='/icon' element={<DefaultHeader />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
