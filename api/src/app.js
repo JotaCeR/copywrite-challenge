2c882bbd8a3567de8ed176b910dbcb43d8e86e9e
Juan Roberts <juancroberts31@gmail.com>
2022-07-29 06:02:28 -0300

    Deploy attempt

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const routes = require('./routes/index')
 const cors = require('cors')
 const server = express()
 
 server.name = 'API'
 
-server.use(morgan('dev'))
+// server.use(morgan('dev'))
 server.use(express.json({ limit: '50mb' }))
 server.use(express.urlencoded({ limit: '50mb', extended: false }))
 server.use(cors())
--- client/src/config/api/index.js
+++ client/src/config/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseConfig = {
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://heroku-reverse-api.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
   }
